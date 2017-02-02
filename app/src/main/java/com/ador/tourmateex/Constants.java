87110d04c7d9eb762c72aa3c8102b52aa2cb3104
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2017-02-03 00:22:58 +0600

    Successfully Registered with server....

--- app/src/main/java/com/ador/tourmateex/Constants.java
+++ app/src/main/java/com/ador/tourmateex/Constants.java
@@ -5,7 +5,8 @@ package com.ador.tourmateex;
  */
 
 public class Constants {
-    private static final String ROOT_URL = "http://192.168.56.1/Android/v1/";
+    private static final String ROOT_URL = "http://192.168.0.105/Android/v1/";
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+
 
 }
--- app/src/main/java/com/ador/tourmateex/MainActivity.java
+++ app/src/main/java/com/ador/tourmateex/MainActivity.java
@@ -1,13 +1,94 @@
 package com.ador.tourmateex;
 
+import android.app.ProgressDialog;
+import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText userNameET,passwordET,emailET;
+    private Button signBtn;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userNameET= (EditText) findViewById(R.id.userNameET);
+        passwordET= (EditText) findViewById(R.id.passwordET);
+        emailET= (EditText) findViewById(R.id.emailET);
+        signBtn= (Button) findViewById(R.id.signBtn);
+
+        progressDialog= new ProgressDialog(this);
+
+        signBtn.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+        final String userName = userNameET.getText().toString().trim();
+        final String password = passwordET.getText().toString().trim();
+        final String email = emailET.getText().toString().trim();
+
+        progressDialog.setMessage("Registering User...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(StringRequest.Method.POST, Constants.URL_REGISTER, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.hide();
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String>params = new HashMap<>();
+                params.put("username",userName);
+                params.put("password",password);
+                params.put("email",email);
+
+
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == signBtn)
+            registerUser();
     }
 }
