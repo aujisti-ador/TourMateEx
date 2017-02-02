4a6d4d1a1afd9c027bb1d5611d7822e1476ec9af
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2017-02-02 12:24:54 +0600

    Initial commit

---
+++ app/src/main/java/com/ador/tourmateex/Constants.java
@@ -0,0 +1,11 @@
+package com.ador.tourmateex;
+
+/**
+ * Created by ADOR'S Lappy on 02-Feb-17.
+ */
+
+public class Constants {
+    private static final String ROOT_URL = "http://192.168.56.1/Android/v1/";
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+
+}
